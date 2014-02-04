326a3d9fc4acc98b98886985a56c753ebe691852
Benjamin Naecker <benjamin.naecker@gmail.com>
2014-02-04 11:25:33 -0800

    Ignoring .pyc files now. Updated binary reader to use fromfile instead of struct.unpack. Several other things changed, but committing now, as I will probably do major reogranizing in an attempt to fully switch analysis pipeline, stimulus database, etc to Python.

--- dtools.py
+++ dtools.py
@@ -5,12 +5,12 @@
 # (c) 2014 bnaecker@stanford.edu
 
 ## Imports
-import scipy as sp				# For lots of stuff
-from scipy.io import loadmat	# Loading MATLAB files
-from os import path, listdir	# Finding files
-import re						# Matching filenames mostly
+from scipy import loadtxt, zeros, fromfile, dtype	# Loading, preallocating data
+from scipy.io import loadmat						# Loading MATLAB files
+from os import listdir								# Finding files
+from os.path import join, expanduser, exists		# Pathnames
+from re import match								# For matching filenames
 
-## Load data about an experiment
 def loadexp(name, date):
 	'''
 	Loads the stimulus and VBL timestamp arrays from the given experiment.
@@ -23,17 +23,16 @@ def loadexp(name, date):
 		VBL arrays and stimuli for the given experiment, as a dictionary
 	'''
 	# Path information
-	datadir = path.join(\
-			path.expanduser('~/FileCabinet/stanford/baccuslab/projects/'), \
+	datadir = join(\
+			expanduser('~/FileCabinet/stanford/baccuslab/projects/'), \
 			name, 'data', date)
 
 	# Load MAT-file for Python
-	pydat = loadmat(path.join(datadir, 'pydat.mat'));
+	pydat = loadmat(join(datadir, 'pydat.mat'));
 
 	# Return the useful keys and arrays
 	return {k: pydat.get(k) for k in pydat.keys() if not k.startswith('__')}
 
-## Load the spikes times of an experiment
 def loadspikes(name, date):
 	'''
 	Loads the spike times for each cell from the given experiment.
@@ -46,15 +45,15 @@ def loadspikes(name, date):
 		spikes	- list of spike times for each cell
 	'''
 	# Path information
-	datadir = path.join(\
-			path.expanduser('~/FileCabinet/stanford/baccuslab/projects/'), \
+	datadir = join(\
+			expanduser('~/FileCabinet/stanford/baccuslab/projects/'), \
 			name, 'data', date)
 
 	# Find the cell files
-	cfiles = [path.join(datadir, f) for f in listdir(datadir) if re.match('c[0-9]*.txt', f)]
+	cfiles = [join(datadir, f) for f in listdir(datadir) if match('c[0-9]*.txt', f)]
 
 	# Return a list of all the cells
-	return [sp.loadtxt(cf) for cf in cfiles]
+	return [loadtxt(cf) for cf in cfiles]
 
 def readigorbin(fname, chan = 0):
 	'''
@@ -65,10 +64,15 @@ def readigorbin(fname, chan = 0):
 		chan	- which channel to read, defaults to 0, the photodiode
 	
 	OUTPUT:
-		hdr		- the file header
+		hdr		- the file header, a Python dict
 		dat		- the channel data, as a numpy ndarray
 	'''
-	if not path.exists(fname):
+
+	# Some read constants
+	uint = dtype('>i2')		# 16-bit unsigned integers
+
+	# Check file exists
+	if not exists(fname):
 		print('Requested bin file {f} does not exist'.format(f = fname))
 		raise FileNotFoundError
 
@@ -78,35 +82,38 @@ def readigorbin(fname, chan = 0):
 		# Rewind the file
 		fid.seek(0)
 
-		# Reade the header
+		# Read the header
 		hdr = readigorhdr(fid)
 
-		# Check that the requested channel is OK
+		# Check that the requested channel is in the recording
 		if chan not in hdr['channel']:
 			print('Requested channel {c} is not in the file'.format(c = chan))
 			raise IndexError
 
-		# A bit more setup
-		nblocks = int(hdr['nsamples'] / hdr['blksize'])
-		skip = hdr['nchannels'] * hdr['blksize']
-		chanoffset = chan * hdr['blksize']
+		# Compute number of blocks and some offsets
+		nblocks = int(hdr['nsamples'] / hdr['blksize']) * uint.itemsize
+		skip = hdr['nchannels'] * hdr['blksize'] * uint.itemsize
+		chanoffset = chan * hdr['blksize'] * uint.itemsize
 
-		# Setup ndarray to return the values
-		dat = sp.zeros((hdr['nsamples'] / hdr['nchannels'],))
+		# Preallocate ndarray to return the values
+		dat = zeros((hdr['nsamples'],))
 
 		# Read the requested channel, a block at a time
 		fid.seek(hdr['hdrsize'])
 		for block in range(nblocks):
+			# Compute start of the block and set the file position
 			pos = hdr['hdrsize'] + block * skip + chanoffset
 			fid.seek(pos)
+
+			# Read the data
 			dat[block * hdr['blksize'] : (block + 1) * hdr['blksize']] = \
-				sp.fromfile(fid, dtype = sp.dtype('>i2'), count = hdr['blksize'])
+				fromfile(fid, dtype = uint, count = hdr['blksize'])
 
-		# Scale and offset
-		dat *= hdr['gain']
-		dat += hdr['offset']
+	# Scale and offset
+	dat *= hdr['gain']
+	dat += hdr['offset']
 
-	# Return stuff
+	# Return the header and the actual data
 	return hdr, dat
 
 def readigorhdr(fid):
@@ -120,36 +127,35 @@ def readigorhdr(fid):
 		hdr		- dict of the header information
 	'''
 
-	# Define some data types
-	uint 	= sp.dtype('>u4') 	# Unsigned integer, 32-bit
-	short 	= sp.dtype('>i2') 	# Signed 16-bit integer
-	flt 	= sp.dtype('>f4') 	# Float, 32-bit
-	uchar 	= sp.dtype('>B') 	# Unsigned char
+	# Define datatypes to be read in
+	uint 	= dtype('>u4') 	# Unsigned integer, 32-bit
+	short 	= dtype('>i2') 	# Signed 16-bit integer
+	flt 	= dtype('>f4') 	# Float, 32-bit
+	uchar 	= dtype('>B') 	# Unsigned char
 
 	# Read the header
 	hdr = {}
-	hdr['hdrsize'] 		= sp.fromfile(fid, dtype = uint, count = 1) 	# size of header (bytes)
-	hdr['type']			= sp.fromfile(fid, dtype = short, count = 1)	# not sure
-	hdr['version']		= sp.fromfile(fid, dtype = short, count = 1) 	# not sure
-	hdr['nsamples']		= sp.fromfile(fid, dtype = uint, count = 1) 	# samples in file
-	hdr['nchannels']	= sp.fromfile(fid, dtype = uint, count = 1) 	# number of channels
-	hdr['channel'] 		= sp.fromfile(fid, dtype = short, count = hdr['nchannels']) 	# channels
-	hdr['fs']			= sp.fromfile(fid, dtype = flt, count = 1)		# sample rate
-	hdr['blksize'] 		= sp.fromfile(fid, dtype = uint, count = 1)		# sz of data blocks
-	hdr['gain']			= sp.fromfile(fid, dtype = flt, count = 1)		# amplifier gain
-	hdr['offset']		= sp.fromfile(fid, dtype = flt, count = 1)		# amplifier offset
-	hdr['datesz']		= sp.fromfile(fid, dtype = uint, count = 1)		# size of date string
-	tmpdate				= sp.fromfile(fid, dtype = uchar, count = hdr['datesz'])		# date
-	hdr['timesz']		= sp.fromfile(fid, dtype = uint, count = 1)		# size of time string
-	tmptime				= sp.fromfile(fid, dtype = uchar, count = hdr['timesz'])		# time
-	hdr['roomsz']		= sp.fromfile(fid, dtype = uint, count = 1)		# size of room string
-	tmproom				= sp.fromfile(fid, dtype = uchar, count = hdr['roomsz'])		# room
+	hdr['hdrsize'] 		= fromfile(fid, dtype = uint, count = 1) 				# size of header (bytes)
+	hdr['type']			= fromfile(fid, dtype = short, count = 1)				# not sure
+	hdr['version']		= fromfile(fid, dtype = short, count = 1) 				# not sure
+	hdr['nsamples']		= fromfile(fid, dtype = uint, count = 1) 				# samples in file
+	hdr['nchannels']	= fromfile(fid, dtype = uint, count = 1) 				# number of channels
+	hdr['channel'] 		= fromfile(fid, dtype = short, count = hdr['nchannels'])# channels
+	hdr['fs']			= fromfile(fid, dtype = flt, count = 1)					# sample rate
+	hdr['blksize'] 		= fromfile(fid, dtype = uint, count = 1)				# sz of data blocks
+	hdr['gain']			= fromfile(fid, dtype = flt, count = 1)					# amplifier gain
+	hdr['offset']		= fromfile(fid, dtype = flt, count = 1)					# amplifier offset
+	hdr['datesz']		= fromfile(fid, dtype = uint, count = 1)				# size of date string
+	tmpdate				= fromfile(fid, dtype = uchar, count = hdr['datesz'])	# date
+	hdr['timesz']		= fromfile(fid, dtype = uint, count = 1)				# size of time string
+	tmptime				= fromfile(fid, dtype = uchar, count = hdr['timesz'])	# time
+	hdr['roomsz']		= fromfile(fid, dtype = uint, count = 1)				# size of room string
+	tmproom				= fromfile(fid, dtype = uchar, count = hdr['roomsz'])	# room
 
-	# Convert the strings to actual strings
+	# Convert the date, time and room to strings
 	hdr['date'] = ''.join([chr(i) for i in tmpdate])
 	hdr['time'] = ''.join([chr(i) for i in tmptime])
 	hdr['room'] = ''.join([chr(i) for i in tmproom])
 
 	# Return the header
 	return hdr
-
--- lntools.py
+++ lntools.py
@@ -7,6 +7,7 @@
 ## Imports
 import scipy as sp
 import matplotlib.pyplot as plt
+import seaborn
 
 ## Bin spikes
 def binspikes(spikes, tmax, binsize=0.01):
@@ -24,10 +25,10 @@ def binspikes(spikes, tmax, binsize=0.01):
 	'''
 	tbins = sp.arange(0, tmax, binsize)
 	bspikes = []
-	for cell in st:
+	for cell in spikes:
 		hist, tax = sp.histogram(cell, bins = tbins)
 		bspikes.append(hist)
-	return binspikes, tax
+	return bspikes, tax
 
 def estimatefr(bspikes, nfiltpts = 7, filtsd = 2):
 	'''
@@ -70,20 +71,19 @@ def ste_spatialwn(stim, vbl, st, nframes = 25):
 	# Loop over cells
 	for cell in st:
 
-		print('working on cell {c} '.format(c = st.index(cell)), end = '')
-		
-		# Construct list to hold this cell's STE
-		thisste = []
-
 		# Number of spikes
 		nspikes = len(cell)
-		si = 1
+		si = 0
+
+		# Preallocate ndarray to hold the STE
+		thisste = sp.zeros((nspikes, stim.shape[0] * stim.shape[1] * nframes))
+
+		# Notify
+		print(' cell {c} of {nc} ({ns} spikes) ...'.format( \
+				c = st.index(cell) + 1, nc = len(st), ns = nspikes), end = '', flush = True)
 
 		# Loop over spikes
 		for spike in cell:
-
-			if sp.mod(si, nspikes / 10) == 0:
-				print('.', end = '')
 
 			# Find the block number
 			block = sp.where(sp.logical_and(vbl[0, :] < spike, spike <= vbl[-1, :]))[0]
@@ -92,18 +92,38 @@ def ste_spatialwn(stim, vbl, st, nframes = 25):
 			frame = sp.sum(sp.where((vbl[:, block] - spike) < 0, 1, 0)) - 1
 
 			# Add the stimulus of the preceding frames, vectorized
-			thisste.append(stim[:, :, frame : frame - nframes : -1].flatten())
+			if frame < nframes:
+				continue
+			thisste[si, :] = stim[:, :, frame : frame - nframes : -1].flatten()
+
+			# Increment spike-counter
+			si += 1
 
 		# Append this cell's STE as an nd-array to the full STE
-		ste.append(sp.asarray(thisste))
-		print('done.')
+		ste.append(thisste)
+		print(' done.', flush = True)
 
 	# Return the STE
 	return ste
 
-def tfsta(tfwn, tfvbl, spikes, upfact = 2, nbands = 3, length = 25):
+def tfsta(tfwn, tfvbl, spikes, upfact = 2, nbands = 3, length = 25, donorm = False):
 	'''
 	Compute the spike-triggered averages within each tfsens block
+
+	INPUT:
+		tfwn	- the central temporal white noise stimulus
+		tfvbl	- array of VBL timestamps during the stimulus
+		upfact	- upsample factor. the VBL arrays and stimulus are upsampled to provide better
+				  estimates of the linear filters.
+		nbands	- number of frequency bands in the stimulus
+		length	- number of points in the filter (true value is multiplied by upfact)
+		donorm	- flag to normalize each filter as a unit vector, defaults to False
+
+	OUTPUT:
+		ste		- the spike-triggered ensemble. a list, with one list of ndarrays for each cell
+				  containing the full spike-triggered ensemble for each frequency band
+		sta		- the linear filters. a list, with one nbands-by-length ndarray, with the linear
+				  filter for each frequency band
 	'''
 
 	# Lists to hold the STA and STE
@@ -117,6 +137,11 @@ def tfsta(tfwn, tfvbl, spikes, upfact = 2, nbands = 3, length = 25):
 
 	# loop over cells
 	for cell in spikes:
+
+		# Notify
+		print(' cell {c} of {nc} ({ns} spikes) ... '.format( \
+				c = spikes.index(cell) + 1, nc = len(spikes), ns = len(cell)), \
+				end = '', flush = True)
 
 		# Loop over blocks
 		nblocks = tfvbl.shape[-1]
@@ -147,19 +172,90 @@ def tfsta(tfwn, tfvbl, spikes, upfact = 2, nbands = 3, length = 25):
 		stearrays = [sp.asarray(st) for st in thisste]
 		#raise Exception
 
-		# Append to the list of all STE's and compute the STA's
+		# Append to the list of all STE's
 		ste.append(stearrays)
-		sta.append([sp.mean(st, axis = 0) for st in stearrays])
+
+		# STA is the mean of the STE. Join into single ndarray, potentially normalizing each
+		if donorm:
+			unnormed = [sp.mean(st, axis = 0) for st in stearrays]
+			filt = sp.asarray([un / sp.linalg.norm(un) for un in unnormed]).T
+		else:
+			filt = sp.asarray([sp.mean(st, axis = 0) for st in stearrays]).T
+
+		# Mean-subtract and append to the STA array
+		sta.append(filt - (sp.outer(sp.ones((length * upfact,)), sp.mean(filt, axis = 0))))
+
+		# Notify
+		print('done.', flush = True)
 
 	# Return the STE and STA lists
 	return ste, sta
+
+def tflinearpred(filters, stim, fpc = 1500, upfact = 2):
+	'''
+	Compute the linear prediction of each TF sensitization filter with the stimulus
+	'''
+	ncells = len(filters)
+	nbands = filters[0].shape[-1]
+	lp = [sp.zeros((fpc * upfact,nbands,nbands)) for i in range(ncells)]
+	upstim = sp.kron(stim, sp.ones((upfact,))).flatten()
+	for ci in range(ncells):
+		for bi in range(nbands):
+			for bj in range(nbands):
+				lp[ci][:, bi, bj] = sp.convolve(upstim[bi * fpc * upfact : (bi + 1) * fpc * upfact], filters[ci][:, bj], 'same')
+
+	return lp
+
+def tfnonlin(filters, lp, ste, nbands = 3, nbins = 30, eps = 1e-3):
+	'''
+	Compute nonlinearities in each frequency band for the temporal frequency sensitization experiment
+	'''
+	ncells = len(filters)
+	nonlin = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
+	sbins = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
+	stbins = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
+	sdist = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)] 
+	stdist = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)] 
+	for ci in range(ncells):
+		for bi in range(nbands):
+			for bj in range(nbands):
+
+				# Get white noise linear prediction, compute histogram (unconditional)
+				pred = lp[ci][:, bi, bj]
+				zpred = sp.stats.zscore(pred)
+				mu = sp.mean(pred)
+				sd = sp.std(pred)
+				sdist[ci][:, bi, bj], bins = sp.histogram(pred, nbins, density = True)
+				sbins[ci][:, bi, bj] = bins[:-1]
+
+				# Compute spike-triggered distribution
+				zst = (ste[ci][bi] - mu) / sd
+				stdist[ci][:, bi, bj], bins = sp.histogram(sp.dot(zst, filters[ci][:, bj]), nbins, density = True)
+				stbins[ci][:, bi, bj] = bins[:-1]
+
+				# Compute nonlinearity, ratio of distributions
+				nonlin[ci][:, bi, bj] = sp.divide(stdist[ci][:, bi, bj], sdist[ci][:, bi, bj] + eps)
+
+	return nonlin, sdist, sbins, stdist, stbins
+
+def plotnl(bins, nl, c = 0, nbands = 3):
+	'''
+	plot the nonlinearities for the given cell
+	'''
+	fig = plt.figure()
+	fig.canvas.manager.set_window_title('cell ' + str(c))
+	for bi in range(nbands):
+		for bj in range(nbands):
+			plt.subplot(nbands, nbands, bi * 3 + bj + 1)
+			plt.plot(bins[c][:, bi, bj], nl[c][:, bi, bj])
+			plt.title('stim: ' + str(bi) + ', filt: ' + str(bj))
 
 def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
 	'''
 	Plot linear filters from the temporal frequency sensitization experiment.
 
 	INPUT:
-		filt		- list of filters for each cell. each cell is a list of filters for each band
+		filt		- list of filters for each cell. each is an ndarray of filters for each band
 		nbands		- number of frequency bands
 		ifi			- monitor inter-frame interval (for plotting time axis)
 		waitframes	- number of frames between monitor flips (also for time axis)
@@ -167,6 +263,10 @@ def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
 	OUTPUT:
 		none
 	'''
+	from matplotlib import cm
+	colors = cm.Blues(tuple(int(i) for i in \
+			sp.linspace(cm.Blues.N / 4, cm.Blues.N - cm.Blues.N / 4, nbands)))
+	
 	# Make a figure
 	plt.figure()
 
@@ -175,7 +275,7 @@ def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
 	nplots = [int(sp.sqrt(ncells)), ncells / int(sp.sqrt(ncells))]
 
 	# Make time axis
-	flength = len(filt[0][0])
+	flength = len(filt[0])
 	tax = sp.linspace(0, flength * ifi * waitframes / upfact, flength)
 
 	# Loop over cells
@@ -184,11 +284,9 @@ def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
 		# Make a subplot
 		plt.subplot(nplots[0], nplots[1], ci + 1)
 
-		# Loop over frequency bands
-		for bi in sp.arange(nbands):
-
-			# Plot each filter
-			plt.plot(tax, filt[ci][bi], linewidth = 2)
+		# Plot all filters for each cell
+		for fi in sp.arange(nbands):
+			plt.plot(tax, filt[ci][:, fi], linewidth = 2, color = colors[fi, :])
 
 		# Labels etc
 		locs, labels = plt.yticks()
@@ -196,9 +294,37 @@ def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
 		plt.title('cell {c}'.format(c = ci + 1))
 
 		# x-labels only on the bottom row
-		if  (ci + 1) >= (nplots[0] - 1) * nplots[1]:
+		if  (ci + 1) > (nplots[0] - 1) * nplots[1]:
 			plt.xlabel('Time before spike (s)')
 
 	# Legend, on the last plot
 	plt.legend(('low', 'mid', 'high'))
 	return
+
+def plotstimfreq(stim, fpc = 1500, ifi = 0.01, waitframes = 2):
+	'''
+	Plot the frequency spectrum of the TF sensitization stimulus
+	'''
+	# Compute sample rate, times, axes
+	T = ifi * waitframes
+	Fs = 1 / T
+	L = fpc
+	tax = sp.arange(0, L) * T
+
+	# Compute FFT, normalized to the length
+	nfft = 2 ** int(sp.ceil(sp.log2(L)))
+	ft = sp.fft(stim, n = nfft, axis = 0) / L
+	f = Fs / 2 * sp.linspace(0, 1, nfft / 2)
+	ps = 2 * sp.absolute(ft[0 : nfft / 2, :])
+	
+	# Smooth power spectrum a little
+	from scipy.signal import lfilter
+	npts = 20
+	sps = lfilter(sp.ones((npts,)) / npts, 1, ps)
+
+	# Plot
+	plt.figure()
+	plt.subplot(211)
+	plt.plot(tax, stim)
+	plt.subplot(212)
+	plt.plot(f[:100], sps[:100, :], linewidth = 2)
