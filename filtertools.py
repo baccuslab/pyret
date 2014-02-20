286859228a0eb29fa528de673b8b6969408c3caf
Benjamin Naecker <benjamin.naecker@gmail.com>
2014-02-19 21:03:07 -0800

    Combined Niru's versions of STE/STA computation from spikeTriggeredAnalysis.py with filtertools module. Removed Ben's old stuff from the way-way-back.

--- filtertools.py
+++ filtertools.py
@@ -1,354 +1,241 @@
 '''
-lntools.py
+filtertools.py
 
-tools for basic linear-nonlinear model components
+tools for computation of basic linear filters
 
 (C) 2014 bnaecker, nirum
 '''
 
-import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from scipy.ndimage.filters import gaussian_filter
 
-def getste(stim, vbl, spk, nframes=25, reshape=False):
-	'''
-	usage: ste = getste(stim, vbl, spk, nframes)
-	construct the spike-triggered ensemble
+def getste(time, stimulus, spikes, filterlength):
+    '''
+    Usage: ste, tax = getste(time, stimulus, spikes, filterlength)
+    Construct the spike-triggered ensemble
 
-	input:
-		stim	- the stimulus array. can be 1-, 2-, or 3-dimensional, corresponding
-				  to temporal, lines, or spatial white noise stimuli
-		vbl		- the vbl timestamp array
-		spk		- list of spike-time arrays for each cell
-		nframes	- number of frames to consider for the ste
-		reshape - reshape the STE to return it as an array of (stimdim1, ..., stimdimN, nframes)
-				  rather than the default prod(stimsize) * nframes
+    Input
+    -----
 
-	output:
-		ste		- the spike-triggered ensemble, same shape as stim
-	'''
-	if sp.ndim(stim) == 3:
-		ste = _ste_spatialwn(stim, vbl, spk, nframes, reshape)
-	elif sp.ndim(stim) == 2:
-		ste = _ste_lineswn(stim, vbl, spk, nframes, reshape)
-	elif sp.ndim(stim) == 1:
-		ste = _ste_temporalwn(stim, vbl, spk, nframes, reshape)
+    time:
+        The time axis of the stimulus
 
-	return ste
+    stimulus:
+        The stimulus array. The first dimension of the stimulus
+        should be the time axis (and be of the same length as
+        the `time` array), but no other restrictions are placed
+        on the shape of the array. In other words, it works for
+        temporal or spatiotemporal stimuli.
 
-def _ste_spatialwn(stim, vbl, spk, nframes, reshape):
-	'''
-	spike-triggered ensemble for spatial white noise stimuli. not called directly, 
-	only through lntools.getste
-	'''
-	# Return list
-	ncells = len(spk)
-	ste = []
+    spikes:
+        Array of spike times.
 
-	# Loop over cells
-	for cell in spk:
+    filterlength:
+        Integer number of frames over which to construct the
+        ensemble
 
-		# Capture white noise spikes only
-		wnspikes = cell[sp.logical_and(cell > vbl[0], cell <= vbl[-1])]
-		nspikes = wnspikes.size
+    Output
+    ------
 
-		# Prealloc array to hold STE
-		wnste = sp.zeros((nspikes, stim.shape[0] * stim.shape[1] * nframes))
+    ste:
+        The spike-triggered stimulus ensemble. The returned array
+        has stimulus.ndim + 1 dimensions, and has a shape of
+        (nspikes, filterlength, stimulus.shape[0]).
 
-		# Notify
-		print(' cell {c} of {nc} ({ns} spikes) ...'.format(\
-				c = spk.index(cell) + 1, nc = ncells, ns = nspikes), \
-				end = '', flush = True)
+    tax:
+        The time axis of the ensemble. It is of length `filterlength`,
+        with intervals given by the sample rate of the `time` input
+        array.
 
-		# Loop over spikes
-		si = 0
-		for spike in wnspikes:
+    '''
 
-			# Find VBL array block
-			block = sp.logical_and(vbl[0, :] < spike, spike <= vbl[-1, :])
+    # Bin spikes
+    (hist, bins) = np.histogram(spikes, time)
 
-			# Add immediately preceding nframes frames
-			frame = sp.sum(vbl[:, block] < spike) - 1
-			if frame < nframes:
-				continue
-			wnste[si, :] = stim[:, :, frame : frame - nframes : -1].flatten()
-			si += 1
+    # Get indices of non-zero firing, truncating spikes earlier
+    # than `filterlength` frames
+    nzhist = np.where(hist > 0)[0]
+    nzhist = nzhist[nzhist > filterlength]
 
-		# Append this ste to the list
-		ste.append(wnste)
-		print(' done.', flush = True)
+    # Collapse any spatial dimensions of the stimulus array
+    cstim = stimulus.reshape(stimulus.shape[0], -1)
 
-	if reshape:
-		return [s.reshape(s.shape[0], stim.shape[0], stim.shape[1], nframes)\
-				for s in ste]
-	return ste
-			
-def _ste_lineswn(stim, vbl, spk, nframes):
-	'''
-	spike-triggered ensemble for lines white noise stimuli. not called directly, 
-	only through lntools.getste
-	'''
-	raise NotImplemented
+    # Preallocate STE array
+    ste = np.empty((nzhist.size, filterlength, cstim.shape[0]))
 
-def _ste_temporalwn(stim, vbl, spk, nframes):
-	'''
-	spike-triggered ensemble for temporal white noise stimuli. not called directly, 
-	only through lntools.getste
-	'''
-	raise NotImplemented
+    # Add filterlength frames preceding each spike to the STE array
+    for idx, val in enumerate(nzhist):
+        ste[idx, :, :] = hist[val] * stimulus[val - filterlenth : val, :]
 
-def tfsta(tfwn, tfvbl, spikes, upfact = 2, nbands = 3, length = 25, donorm = False):
-	'''
-	Compute the spike-triggered averages within each tfsens block
+    # Construct a time axis to return
+    tax = time[:filterlength] - time[0]
 
-	INPUT:
-		tfwn	- the central temporal white noise stimulus
-		tfvbl	- array of VBL timestamps during the stimulus
-		upfact	- upsample factor. the VBL arrays and stimulus are upsampled to provide better
-				  estimates of the linear filters.
-		nbands	- number of frequency bands in the stimulus
-		length	- number of points in the filter (true value is multiplied by upfact)
-		donorm	- flag to normalize each filter as a unit vector, defaults to False
+    # Return reshaped STE and the time axis
+    return ste.reshape(nzhist.size, stimulus.shape), tax
 
-	OUTPUT:
-		ste		- the spike-triggered ensemble. a list, with one list of ndarrays for each cell
-				  containing the full spike-triggered ensemble for each frequency band
-		sta		- the linear filters. a list, with one nbands-by-length ndarray, with the linear
-				  filter for each frequency band
-	'''
+def getsta(time, stimulus, spikes, filterlength):
+    '''
+    Usage: sta, tax = getste(time, stimulus, spikes, filterlength)
+    Compute the spike-triggered average
 
-	# Lists to hold the STA and STE
-	sta, ste = [], []
+    Input
+    -----
 
-	# Compute frames per condition
-	fpc = tfvbl.shape[0] / nbands
+    time:
+        The time axis of the stimulus
 
-	# Upsample the white noise stimulus
-	upwn = sp.kron(tfwn, sp.ones((upfact,))).flatten()
+    stimulus:
+        The stimulus array. The first dimension of the stimulus
+        should be the time axis (and be of the same length as
+        the `time` array), but no other restrictions are placed
+        on the shape of the array. In other words, it works for
+        temporal or spatiotemporal stimuli.
 
-	# loop over cells
-	for cell in spikes:
+    spikes:
+        Array of spike times.
 
-		# Notify
-		print(' cell {c} of {nc} ({ns} spikes) ... '.format( \
-				c = spikes.index(cell) + 1, nc = len(spikes), ns = len(cell)), \
-				end = '', flush = True)
+    filterlength:
+        Integer number of frames over which to construct the
+        ensemble
 
-		# Loop over blocks
-		nblocks = tfvbl.shape[-1]
-		thisste = [[] for i in sp.arange(0, nbands)]
-		for block in sp.arange(0, nblocks):
+    Output
+    ------
 
-			# Loop over each frequency band
-			for bi in sp.arange(0, nbands):
+    sta:
+        The spike-triggered average. The returned array has 
+        stimulus.ndim + 1 dimensions, and has a shape of
+        (nspikes, stimulus.shape[0], filterlength).
 
-				# Upsample VBL array
-				condstart = bi * fpc
-				condend = (bi + 1) * fpc
-				vbl = tfvbl[condstart : condend, block]
-				upvbl = sp.interp(sp.arange(0, fpc * upfact), 
-						sp.arange(0, fpc * upfact, upfact), vbl)
+    tax:
+        The time axis of the ensemble. It is of length `filterlength`,
+        with intervals given by the sample rate of the `time` input
+        array.
 
-				# Loop over spikes from this block
-				spk = cell[sp.where(sp.logical_and(cell > upvbl[0], cell <= upvbl[-1]))]
-				for spike in spk:
-					# Get immediately preceding frame
-					frame = sp.sum(sp.where(upvbl < spike, 1, 0)) - 1 + bi * fpc * upfact
-					if frame < length * upfact:
-						continue
-					# Append the preceding length frames to the STE
-					thisste[bi].append(upwn[frame : frame - length * upfact : -1])
-	
-		# Create nd-array from the spike-triggered stimulus ensemble list
-		stearrays = [sp.asarray(st) for st in thisste]
-		#raise Exception
+    '''
 
-		# Append to the list of all STE's
-		ste.append(stearrays)
+    # Bin spikes
+    (hist, bins) = np.histogram(spikes, time)
 
-		# STA is the mean of the STE. Join into single ndarray, potentially normalizing each
-		if donorm:
-			unnormed = [sp.mean(st, axis = 0) for st in stearrays]
-			filt = sp.asarray([un / sp.linalg.norm(un) for un in unnormed]).T
-		else:
-			filt = sp.asarray([sp.mean(st, axis = 0) for st in stearrays]).T
+    # Get indices of non-zero firing, truncating spikes earlier
+    # than `filterlength` frames
+    nzhist = np.where(hist > 0)[0]
+    nzhist = nzhist[nzhist > filterlength]
 
-		# Mean-subtract and append to the STA array
-		sta.append(filt - (sp.outer(sp.ones((length * upfact,)), sp.mean(filt, axis = 0))))
+    # Collapse any spatial dimensions of the stimulus array
+    cstim = stimulus.reshape(stimulus.shape[0], -1)
 
-		# Notify
-		print('done.', flush = True)
+    # Preallocate STA array
+    sta = np.empty((filterlength, cstim.shape[1]))
 
-	# Return the STE and STA lists
-	return ste, sta
+    # Add filterlength frames preceding each spike to the running STA
+    for idx in nzhist:
+        sta += hist[idx] * stimulus[idx - filterlength : idx, :]
 
-def tflinearpred(filters, stim, fpc = 1500, upfact = 2):
-	'''
-	Compute the linear prediction of each TF sensitization filter with the stimulus
-	'''
-	ncells = len(filters)
-	nbands = filters[0].shape[-1]
-	lp = [sp.zeros((fpc * upfact,nbands,nbands)) for i in range(ncells)]
-	upstim = sp.kron(stim, sp.ones((upfact,))).flatten()
-	for ci in range(ncells):
-		for bi in range(nbands):
-			for bj in range(nbands):
-				lp[ci][:, bi, bj] = sp.convolve(upstim[bi * fpc * upfact : (bi + 1) * fpc * upfact], filters[ci][:, bj], 'same')
+    # Construct a time axis to return
+    tax = time[:filterlength] - time[0]
 
-	return lp
+    # Return reshaped STA and the time axis
+    return sta.reshape(filterlength, stimulus.shape[1:]), tax
 
-def tfnonlin(filters, lp, ste, nbands = 3, nbins = 30, eps = 1e-3):
-	'''
-	Compute nonlinearities in each frequency band for the temporal frequency sensitization experiment
-	'''
-	ncells = len(filters)
-	nonlin = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
-	sbins = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
-	stbins = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)]
-	sdist = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)] 
-	stdist = [sp.zeros((nbins, nbands, nbands)) for ci in range(ncells)] 
-	for ci in range(ncells):
-		for bi in range(nbands):
-			for bj in range(nbands):
+def lowranksta(f, k=10):
+    '''
+    Usage: fk, u, s, v = lowranksta(f, k=10)
+    Decomposes a 3D spatiotemporal filter into the outer product 
+    of spatial and temporal components (via the SVD). This is useful,
+    for example, in computing the spatial and temporal kernels of a
+    spatiotemporal STA, or in denoising the STA.
 
-				# Get white noise linear prediction, compute histogram (unconditional)
-				pred = lp[ci][:, bi, bj]
-				zpred = sp.stats.zscore(pred)
-				mu = sp.mean(pred)
-				sd = sp.std(pred)
-				sdist[ci][:, bi, bj], bins = sp.histogram(pred, nbins, density = True)
-				sbins[ci][:, bi, bj] = bins[:-1]
+    Input
+    -----
 
-				# Compute spike-triggered distribution
-				zst = (ste[ci][bi] - mu) / sd
-				stdist[ci][:, bi, bj], bins = sp.histogram(sp.dot(zst, filters[ci][:, bj]), nbins, density = True)
-				stbins[ci][:, bi, bj] = bins[:-1]
+    f:
+        3-D filter to be separated
 
-				# Compute nonlinearity, ratio of distributions
-				nonlin[ci][:, bi, bj] = sp.divide(stdist[ci][:, bi, bj], sdist[ci][:, bi, bj] + eps)
+    k:
+        number of components to keep (rank of the filter)
 
-	return nonlin, sdist, sbins, stdist, stbins
+    Output
+    ------
 
-def plotnl(bins, nl, c = 0, nbands = 3):
-	'''
-	plot the nonlinearities for the given cell
-	'''
-	fig = plt.figure()
-	fig.canvas.manager.set_window_title('cell ' + str(c))
-	for bi in range(nbands):
-		for bj in range(nbands):
-			plt.subplot(nbands, nbands, bi * 3 + bj + 1)
-			plt.plot(bins[c][:, bi, bj], nl[c][:, bi, bj])
-			plt.title('stim: ' + str(bi) + ', filt: ' + str(bj))
+    fk:
+        the rank-k filter
 
-def plottffilters(filt, nbands = 3, ifi = 0.01, waitframes = 2, upfact = 2):
-	'''
-	Plot linear filters from the temporal frequency sensitization experiment.
+    u:
+        the top k spatial components  (each row is a component)
 
-	INPUT:
-		filt		- list of filters for each cell. each is an ndarray of filters for each band
-		nbands		- number of frequency bands
-		ifi			- monitor inter-frame interval (for plotting time axis)
-		waitframes	- number of frames between monitor flips (also for time axis)
+    s:
+        the top k singular values
 
-	OUTPUT:
-		none
-	'''
-	from matplotlib import cm
-	colors = cm.Blues(tuple(int(i) for i in \
-			sp.linspace(cm.Blues.N / 4, cm.Blues.N - cm.Blues.N / 4, nbands)))
-	
-	# Make a figure
-	plt.figure()
+    u:
+        the top k temporal components (each column is a component)
 
-	# Determine number of subplots
-	ncells = len(filt)
-	nplots = [int(sp.sqrt(ncells)), ncells / int(sp.sqrt(ncells))]
+    '''
 
-	# Make time axis
-	flength = len(filt[0])
-	tax = sp.linspace(0, flength * ifi * waitframes / upfact, flength)
+    # Compute the SVD of the full filter
+    u, s, v = svd(f.reshape(-1, f.shape[-1]), full_matrices=False)
 
-	# Loop over cells
-	for ci in sp.arange(ncells):
+    # Keep the top k components
+    k = np.min([k, s.size])
 
-		# Make a subplot
-		plt.subplot(nplots[0], nplots[1], ci + 1)
+    # Compute the rank-k filter
+    fk = (u[:,:k].dot(np.diag(s[:k]).dot(v[:k,:]))).reshape(f.shape)
 
-		# Plot all filters for each cell
-		for fi in sp.arange(nbands):
-			plt.plot(tax, filt[ci][:, fi], linewidth = 2, color = colors[fi, :])
+    # Return the rank-k approximate filter, and the SVD components
+    return fk, u, s, v
 
-		# Labels etc
-		locs, labels = plt.yticks()
-		plt.yticks(locs, [''])
-		plt.title('cell {c}'.format(c = ci + 1))
+def decompose(sta):
+    '''
+    Usage: s, t = decompose(sta)
+    Decomposes a spatiotemporal STA into a spatial and temporal kernel
 
-		# x-labels only on the bottom row
-		if  (ci + 1) > (nplots[0] - 1) * nplots[1]:
-			plt.xlabel('Time before spike (s)')
+    Input
+    -----
 
-	# Legend, on the last plot
-	plt.legend(('low', 'mid', 'high'))
-	return
+    sta:
+        The full 3-dimensional STA to be decomposed
 
-def plotstimfreq(stim, fpc = 1500, ifi = 0.01, waitframes = 2):
-	'''
-	Plot the frequency spectrum of the TF sensitization stimulus
-	'''
-	# Compute sample rate, times, axes
-	T = ifi * waitframes
-	Fs = 1 / T
-	L = fpc
-	tax = sp.arange(0, L) * T
+    Output
+    ------
 
-	# Compute FFT, normalized to the length
-	nfft = 2 ** int(sp.ceil(sp.log2(L)))
-	ft = sp.fft(stim, n = nfft, axis = 0) / L
-	f = Fs / 2 * sp.linspace(0, 1, nfft / 2)
-	ps = 2 * sp.absolute(ft[0 : nfft / 2, :])
-	
-	# Smooth power spectrum a little
-	from scipy.signal import lfilter
-	npts = 20
-	sps = lfilter(sp.ones((npts,)) / npts, 1, ps)
+    s:
+        The spatial kernel
 
-	# Plot
-	plt.figure()
-	plt.subplot(211)
-	plt.plot(tax, stim)
-	plt.subplot(212)
-	plt.plot(f[:100], sps[:100, :], linewidth = 2)
+    t:
+        The temporal kernel
+
+    '''
+    _, u, _, v = lowranksta(f, k=1)
+    return u[:, 0].reshape(sta.shape[:2]), v[:, 0]
 
 def _fit2Dgaussian(histogram, numSamples=1e4):
-	''' Fit 2D gaussian to empirical histogram '''
+    ''' Fit 2D gaussian to empirical histogram '''
 
-	# Indices
-	x = np.linspace(0,1,histogram.shape[0])
-	y = np.linspace(0,1,histogram.shape[1])
-	xx,yy = np.meshgrid(x,y)
+    # Indices
+    x = np.linspace(0,1,histogram.shape[0])
+    y = np.linspace(0,1,histogram.shape[1])
+    xx,yy = np.meshgrid(x,y)
 
-	# Draw samples
-	indices = np.random.choice(np.flatnonzero(histogram+1), size=numSamples, replace=True, p=histogram.ravel())
-	x_samples = xx.ravel()[indices]
-	y_samples = yy.ravel()[indices]
+    # Draw samples
+    indices = np.random.choice(np.flatnonzero(histogram+1), size=numSamples, replace=True, p=histogram.ravel())
+    x_samples = xx.ravel()[indices]
+    y_samples = yy.ravel()[indices]
 
-	# Fit mean / covariance
-	samples = np.array((x_samples,y_samples))
-	centerIdx = np.unravel_index(np.argmax(histogram), histogram.shape)
-	center = (xx[centerIdx], yy[centerIdx])
-	C = np.cov(samples)
+    # Fit mean / covariance
+    samples = np.array((x_samples,y_samples))
+    centerIdx = np.unravel_index(np.argmax(histogram), histogram.shape)
+    center = (xx[centerIdx], yy[centerIdx])
+    C = np.cov(samples)
 
-	# Get width / angles
-	widths,vectors = np.linalg.eig(C)
-	angle = np.arccos(vectors[0,0])
+    # Get width / angles
+    widths,vectors = np.linalg.eig(C)
+    angle = np.arccos(vectors[0,0])
 
-	return center, widths, angle, xx, yy
+    return center, widths, angle, xx, yy
 
 def _im2hist(data, spatialSmoothing = 2.5):
-    """ Converts 2D image to histogram """
+    ''' Converts 2D image to histogram '''
 
     # Smooth the data
     data_smooth = gaussian_filter(data, spatialSmoothing, order=0)
@@ -372,33 +259,33 @@ def _im2hist(data, spatialSmoothing = 2.5):
     return pdf
 
 def getellipse(F, scale=1.5):
-	'''
-	Usage: ell = getellipse(staframe, scale=1.5)
-	Fit an ellipse to the given spatial receptive field
+    '''
+    Usage: ell = getellipse(staframe, scale=1.5)
+    Fit an ellipse to the given spatial receptive field
 
-	Input
-	-----
+    Input
+    -----
 
-	staframe:
-		The spatial receptive field to which the ellipse should be fit
+    staframe:
+            The spatial receptive field to which the ellipse should be fit
 
-	scale:
-		Scale factor for the ellipse
+    scale:
+            Scale factor for the ellipse
 
-	Output
-	------
+    Output
+    ------
 
-	ell:
-		A matplotlib.patches.Ellipse object
+    ell:
+            A matplotlib.patches.Ellipse object
 
-	'''
+    '''
 
-	# Get ellipse parameters
-	histogram = _im2hist(F)
-	center, widths, theta, xx, yy = fit2Dgaussian(histogram, numSamples=1e5)
+    # Get ellipse parameters
+    histogram = _im2hist(F)
+    center, widths, theta, xx, yy = fit2Dgaussian(histogram, numSamples=1e5)
 
-	# Generate ellipse
-	ell = Ellipse(xy=center, width=scale*widths[0], height=scale*widths[1], angle=np.rad2deg(theta)+90)
+    # Generate ellipse
+    ell = Ellipse(xy=center, width=scale*widths[0], height=scale*widths[1], angle=np.rad2deg(theta)+90)
 
-	return ell
+    return ell
 
