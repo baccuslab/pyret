f0f6ae4a64a9f0e2c84b4e8464fbdb7177331ddf
Niru Maheswaranathan <nirum@stanford.edu>
2014-02-21 16:34:19 -0800

    added spiking event class

--- spiketools.py
+++ spiketools.py
@@ -86,3 +86,128 @@ def estfr(bspk, binsize=0.01, npts=7, sd=2):
 
     # Filter  binned spike times
     return signal.lfilter(filt, 1, cell) / binsize
+
+class spikingevent:
+    '''
+
+    The spiking event class bundles together functions that are used to analyze
+    individual firing events, consisting of spiking activity recorded across trials / cells / conditions.
+
+    Properties
+    ----------
+
+    start:
+        the start time of the firing event
+
+    stop:
+        the stop time of the firing event
+
+    spikes:
+        the spikes associated with this firing event. This data is stored as an (n by 2) numpy array,
+        where the first column is the set of spike times in the event and the second column is a list of
+        corresponding trial/cell/condition indices for each spike
+
+    '''
+
+    def __init__(self, startTime, stopTime, spikes):
+        self.start = startTime
+        self.stop = stopTime
+        self.spikes = spikes
+
+    def __repr__(self):
+        '''
+        Printing this object prints out the start / stop time and number of spikes in the event
+        '''
+        return ('%5.2fs - %5.2fs (%i spikes)' % (self.start, self.stop, self.spikes.shape[0]))
+
+    def __eq__(self, other):
+        '''
+        Equality between two spiking events is true if the start & stop times are the same
+        '''
+        return (self.start == other.start) & (self.stop == other.stop)
+
+    def trialCounts(self):
+        '''
+        Count the number of spikes per trial
+
+        Usage: counts = spkevent.trialCounts()
+
+        '''
+        counts, _ = np.histogram(self.spikes[:,1], bins=np.arange(np.min(self.spikes[:,1]), np.max(self.spikes[:,1])))
+        return counts
+
+    def eventStats(self):
+        '''
+        Compute statistics (mean and standard deviation) across trial spike counts
+
+        Usage: mu, sigma = spkevent.trialStats()
+
+        '''
+
+        # count number of spikes per trial
+        counts = self.eventCounts()
+
+        return np.mean(counts), np.std(counts)
+
+    def ttfs(self):
+        '''
+        Computes the time to first spike for each trial, ignoring trials that had zero spikes
+
+        Usage: times = spkevent.ttfs()
+
+        '''
+        (trials, indices) = np.unique(self.spikes[:,1], return_index=True)
+        return self.spikes[indices,0]
+    
+    def jitter(self):
+        '''
+        Computes the jitter (standard deviation) in the time to first spike across trials
+
+        Usage: sigma = spkevent.jitter()
+
+        '''
+        return np.std(self.ttfs())
+
+    def sort(self):
+        '''
+        Sort trial indices by the time to first spike
+
+        Usage: sortedspikes = spkevent.sort()
+
+        '''
+
+        # get first spike in each trial
+        _, trialIndices = np.unique(self.spikes[:,1], return_index=True)
+
+        # sort by time of first spike
+        sortedIndices = np.argsort(self.spikes[trialIndices, 0])
+
+        # get reassigned trials
+        sortedtrials = self.spikes[trialIndices[sortedIndices], 1]
+
+        # store new spiking array, resetting trial numbers to the new index values
+        sortedspikes = self.spikes.copy()
+        for idx in range(sortedtrials.size):
+            sortedspikes[self.spikes[:,1]==sortedtrials[idx],1] = idx+1
+
+        return sortedspikes
+
+    def plot(self, sort=False, ax=None, color='SlateGray'):
+        '''
+        Plots this event, as a spike raster
+
+        Usage: spkevent.plot()
+
+        '''
+
+        if sort:
+            spikes = self.sort()
+        else:
+            spikes = self.spikes
+
+        if not ax:
+            ax = plt.figure().add_subplot(111)
+
+        ax.plot(spikes[:,0], spikes[:,1], 'o', markersize=10, markercolor=color)
+
+
