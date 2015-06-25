1f698c0730d33ac61c88483ba8fea984b4a046ac
Niru Maheswaranathan <nirum@stanford.edu>
2015-06-25 14:14:39 -0700

    added rolling_window function to filtertools. Closes #52

--- filtertools.py
+++ filtertools.py
@@ -15,8 +15,10 @@ from skimage.filters import gaussian_filter
 from scipy.optimize import curve_fit
 from .stimulustools import getcov as _getcov
 
-__all__ = ['getste', 'getsta', 'getstc', 'lowranksta', 'decompose', 'get_ellipse_params',
-           'fit_ellipse', 'filterpeak', 'smoothfilter', 'cutout', 'prinangles']
+__all__ = ['getste', 'getsta', 'getstc', 'lowranksta', 'decompose',
+           'get_ellipse_params', 'fit_ellipse', 'filterpeak', 'smoothfilter',
+           'cutout', 'prinangles', 'rolling_window']
+
 
 def getste(time, stimulus, spikes, filter_length, tproj=None):
     """
@@ -755,3 +757,42 @@ def prinangles(u, v):
     ang = np.rad2deg(np.arccos(mag))
 
     return ang, mag
+
+
+def rolling_window(array, window):
+    """
+    Make an ndarray with a rolling window of the last dimension
+
+    Parameters
+    ----------
+    array : array_like
+        Array to add rolling window to
+    window : int
+        Size of rolling window
+
+    Returns
+    -------
+    Array that is a view of the original array with a added dimension
+    of size w.
+
+    Examples
+    --------
+    >>> x=np.arange(10).reshape((2,5))
+    >>> rolling_window(x, 3)
+    array([[[0, 1, 2], [1, 2, 3], [2, 3, 4]],
+           [[5, 6, 7], [6, 7, 8], [7, 8, 9]]])
+
+    Calculate rolling mean of last dimension:
+
+    >>> np.mean(rolling_window(x, 3), -1)
+    array([[ 1.,  2.,  3.],
+           [ 6.,  7.,  8.]])
+
+    """
+    assert window >= 1, "`window` must be at least 1."
+    assert window < array.shape[-1], "`window` is too long."
+
+    # # with strides
+    shape = array.shape[:-1] + (array.shape[-1] - window, window)
+    strides = array.strides + (array.strides[-1],)
+    return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
