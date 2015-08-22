62b00540feb3d25d9ec6d98df98165f1f778bc12
Niru Maheswaranathan <nirum@stanford.edu>
2015-08-21 20:44:25 -0700

    Added support for partitioning into minibatches

--- pyret/containers.py
+++ pyret/containers.py
@@ -42,6 +42,7 @@ class Experiment(object):
         self.dt = dt
 
         spikes = list()
+        print('Spikes for {:d} cells'.format(len(cells)))
         for cell in self.cells:
             spikes.append(np.append(0, binspikes(cell, time=time)[0]))
         self.spikes = np.vstack(spikes)
@@ -57,19 +58,31 @@ class Experiment(object):
     def ncells(self):
         return self.spikes.shape[0]
 
-    def stim_sliced(self, history):
+    def stim_sliced(self, history, batch_size=-1):
         """Returns a view into the stimulus array"""
-        return np.rollaxis(rolling_window(self.stim, history), 3, 2)
 
-    def spike_history(self, history, offset=1):
+        sliced_array = np.rollaxis(rolling_window(self.stim, history), 3, 2)
+
+        if batch_size > 0:
+            return partition_last(sliced_array, batch_size)
+        else:
+            return sliced_array
+
+    def spike_history(self, history, offset=1, batch_size=-1):
         """Returns a view into the spikes array, offset by some amount"""
-        arr = np.hstack((np.zeros((self.ncells,offset)), self.spikes[:, offset:]))
-        return np.rollaxis(rolling_window(arr, history), 2, 1)
+        arr = np.hstack((np.zeros((self.ncells, offset)),
+                         self.spikes[:, offset:]))
+        sliced_array = np.rollaxis(rolling_window(arr, history), 2, 1)
+
+        if batch_size > 0:
+            return partition_last(sliced_array, batch_size)
+        else:
+            return sliced_array
 
     def ste(self, stim_hist, ci):
         return (self.stim[..., (t-stim_hist):t].astype('float')
                 for t in range(len(self))
-                if self.spikes[ci,t] > 0 and t >= stim_hist)
+                if self.spikes[ci, t] > 0 and t >= stim_hist)
 
 
 class Filter(np.ndarray):
@@ -84,7 +97,8 @@ class Filter(np.ndarray):
 
         # add the new attribute to the created instance
         obj.dt = dt
-        obj.tau = np.linspace(0, -dt*input_array.shape[-1], input_array.shape[-1])
+        obj.tau = np.linspace(0, -dt*input_array.shape[-1],
+                              input_array.shape[-1])
 
         # Finally, we must return the newly created object:
         return obj
@@ -92,3 +106,19 @@ class Filter(np.ndarray):
     # def __array_finalize__(self, obj):
         # if obj is None: return
         # self.info = getattr(obj, 'info', 1)
+
+
+def partition_last(arr, size, keeplast=True):
+
+    if keeplast:
+        n = int(np.ceil(float(arr.shape[-1]) / float(size)))
+    else:
+        n = int(np.floor(float(arr.shape[-1]) / float(size)))
+
+    sliced_array = list()
+
+    for idx in range(n):
+        inds = slice(idx * size, min((idx+1) * size, arr.shape[-1]))
+        sliced_array.append(arr[..., inds])
+
+    return sliced_array
