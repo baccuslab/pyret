37700aa151987d78a753def8d52cf76b496fd741
bnaecker <bnaecker@users.noreply.github.com>
2017-04-26 14:53:37 -0700

    Changes testing strategy for the visualization module. (#92)
    Previous versions tested the module by comparing images. The tests
    generated an image, which was compared pixel-to-pixel with a baseline
    version saved in the repository. This is complicated, error-prone, and
    difficult to maintain.
    
    This commit now tests the module by calling the functions with simple
    and known data, and then verifying that the data in the actual plot
    objects rendered matches our expectations based on the inputs.

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -5,108 +5,214 @@ Testing code for pyret.visualizations module.
 
 import os
 
-from matplotlib.testing.compare import compare_images
+import matplotlib.pyplot as plt
+from matplotlib.image import AxesImage
+from matplotlib.patches import Ellipse
 import numpy as np
 import pytest
 
-from pyret import visualizations as viz
+from pyret import visualizations
 from pyret import spiketools
-import utils # Pyret testing and image generation utilities
-
-def _comparison_wrapper(filename, save_func):
-    """A wrapper function for comparing a test image with the baseline version.
-
-    The actual methods which generate and save the testing (and baseline)
-    images are defined in the `utils` module. These are the functions such
-    as `plot_cells_saver`, which will save the output of 
-    `pyret.visualizations.plot_cells` into the file with the given name.
-
-    This method calls the saver function with the given filename, then
-    compares the baseline version (which has the same name but is in the
-    `test-images/baseline` directory) against the generated test image.
-
-    Parameters
-    ----------
-    
-    filename : str
-        The name of the file (without a path) in which to save the test
-        image.
-
-    save_func : callable
-        The saver function which will generate and save the test image
-        in the passed `filename`.
-    """
-    test_name = os.path.join(utils.get_test_image_dir(), filename)
-    base_name = os.path.join(utils.get_baseline_image_dir(), filename)
-    save_func(test_name)
-    assert not compare_images(test_name, base_name, 1)
-    os.remove(test_name)
+import utils # Pyret testing utilities
 
 
 def test_temporal_filter():
-    """Test plotting a temporal filter from a full 3D spatiotemporal STA."""
-    _comparison_wrapper('temporal-filter.png', utils.temporal_filter_saver)
+    """Test plotting a temporal filter directly."""
+    # Plot filter
+    temporal_filter, _, _ = utils.create_default_fake_filter()
+    time = np.arange(temporal_filter.size)
+    visualizations.temporal(time, temporal_filter)
+
+    # Verify data plotted correctly
+    line = plt.findobj(plt.gca(), plt.Line2D)[0]
+    assert np.all(line.get_xdata() == time), \
+            'Time axis data is incorrect.'
+    assert np.all(line.get_ydata() == temporal_filter), \
+            'Temporal filter data is incorrect.'
+    plt.close(plt.gcf())
 
 
 def test_spatial_filter():
-    """Test plotting a spatial filter from a full 3D spatiotemporal STA."""
-    _comparison_wrapper('spatial-filter.png', utils.spatial_filter_saver)
+    """Test plotting a spatial filter directly."""
+    # Plot filter
+    _, spatial_filter, _ = utils.create_default_fake_filter()
+    visualizations.spatial(spatial_filter)
+    data = spatial_filter - spatial_filter.mean()
+
+    # Verify data plotted correctly
+    img = plt.findobj(plt.gca(), AxesImage)[0]
+    assert np.all(img.get_array() == data), 'Spatial filter data is incorrect.'
+    plt.close(plt.gcf())
+
+    # Verify data plotted correctly when giving a maximum value
+    maxval = np.abs(spatial_filter).max()
+    visualizations.spatial(spatial_filter, maxval=maxval)
+    img = plt.findobj(plt.gca(), AxesImage)[0]
+    assert np.all(img.get_array() == spatial_filter), \
+            'Spatial filter data incorrect when passing explicit maxval'
+    assert np.all(img.get_clim() == np.array((-maxval, maxval))), \
+            'Spatial filter color limits not set correctly.'
+    plt.close(plt.gcf())
 
 
-def test_spatiotemporal_filter():
-    """Test plotting a full 3D spatiotemporal STA."""
+def test_plot_sta():
+    """Test visualizations.plot_sta method."""
     # Test plotting temporal component
-    _comparison_wrapper('temporal-from-full-filter.png', 
-            utils.temporal_from_spatiotemporal_filter_saver)
+    temporal_filter, spatial_filter, sta = utils.create_default_fake_filter()
+    time = np.arange(temporal_filter.size)
+    visualizations.plot_sta(time, temporal_filter)
+    line = plt.findobj(plt.gca(), plt.Line2D)[0]
+    assert np.all(line.get_xdata() == time), 'Time axis data is incorrect.'
+    assert np.all(line.get_ydata() == temporal_filter), 'Temporal filter data is incorrect.'
+    plt.close(plt.gcf())
 
     # Test plotting spatial component
-    _comparison_wrapper('spatial-from-full-filter.png', 
-        utils.spatial_from_spatiotemporal_filter_saver)
+    visualizations.plot_sta(time, spatial_filter)
+    img = plt.findobj(plt.gca(), AxesImage)[0]
+    desired = (spatial_filter - spatial_filter.mean()) / spatial_filter.var()
+    actual = img.get_array()
+    assert np.allclose(actual, desired), 'Spatial filter data is incorrect.'
+    plt.close(plt.gcf())
 
-    # Test plotting both spatial/temporal components
-    _comparison_wrapper('full-filter.png',
-        utils.spatiotemporal_filter_saver)
+    # Test plotting both spatial/temporal components.
+    # This code is a bit suspect. `plot_sta` internally calls 
+    # `filtertools.decompose`, which will find singular vectors that are
+    # unit norm. But then `plot_sta` also calls `spatial`, which does
+    # some of its own normalization. The result is that it's difficult
+    # to know what scale the true data plotted should have, so this test
+    # just normalizes all plots and images.
+    fig, axes = visualizations.plot_sta(time, sta)
+    img = plt.findobj(axes[0], AxesImage)[0]
+    desired = (spatial_filter - spatial_filter.mean())
+    desired /= desired.max()
+    actual = img.get_array()
+    actual /= actual.max()
+    assert np.allclose(actual, desired), 'Spatial filter data is incorrect.'
+
+    line = plt.findobj(axes[1], plt.Line2D)[0]
+    assert np.all(line.get_xdata() == time), 'Time axis data is incorrect.'
+    desired = (temporal_filter - temporal_filter.min())
+    desired /= desired.max()
+    actual = line.get_ydata()
+    actual -= actual.min()
+    actual /= actual.max()
+    assert np.allclose(desired, actual), 'Temporal filter data is incorrect.'
+
+    # Verify raising a value error when incorrect dimensionality passed
+    with pytest.raises(ValueError):
+        visualizations.plot_sta(None, np.random.randn(2, 2, 2, 2))
 
 
 def test_raster():
     """Test plotting a spike raster."""
-    _comparison_wrapper('raster.png', utils.raster_saver)
+    spikes, labels = utils.create_default_fake_spikes()
+    visualizations.raster(spikes, labels)
+    line = plt.findobj(plt.gca(), plt.Line2D)[0]
+    assert np.all(line.get_xdata() == spikes), 'Spike times do not match'
+    assert np.all(line.get_ydata() == labels), 'Spike labels do not match'
+
+    # Verify exception raised when spikes and labels different length
+    with pytest.raises(AssertionError):
+        visualizations.raster(np.array((0, 1)), np.array((0, 1, 2)))
 
 
 def test_psth():
     """Test plotting a PSTH."""
-    _comparison_wrapper('psth.png', utils.psth_saver)
+    spikes, _ = utils.create_default_fake_spikes()
+    visualizations.psth(spikes, trial_length=5.0)
+    line = plt.findobj(plt.gca(), plt.Line2D)[0]
+    xdata, ydata = line.get_data()
+    binsize = 0.01
+    ntrials = 2
+    assert xdata.size == (spikes.size / binsize) / ntrials, \
+            'visualizations.psth did not use trial length correctly'
+    assert ydata.max() == (1 / binsize), \
+            'visualizations.psth did not correctly compute max spike rate'
 
 
 def test_raster_and_psth():
     """Test plotting a raster and PSTH on the same axes."""
-    _comparison_wrapper('raster-and-psth.png', utils.raster_and_psth_saver)
-
+    spikes, _ = utils.create_default_fake_spikes()
+    visualizations.raster_and_psth(spikes, trial_length=5.0)
+    axes = plt.findobj(plt.gcf(), plt.Axes)
+    psth_line = plt.findobj(axes[0], plt.Line2D)[0]
+    raster_lines = plt.findobj(axes[1], plt.Line2D)[:2]
+    
+    binsize = 0.01
+    ntrials = 2
+    assert psth_line.get_xdata().size == (spikes.size / binsize) / ntrials, \
+            'visualizations.raster_and_psth did not use trial length correctly'
+    assert psth_line.get_ydata().max() == (1 / binsize), \
+            'visualizations.raster_and_psth did not correctly compute max spike rate'
+    assert raster_lines[0].get_xdata().size == 5, \
+            'visualizations.raster_and_psth did not correctly split rasters into trials'
+    assert raster_lines[0].get_ydata().size == 5, \
+            'visualizations.raster_and_psth did not correctly split rasters into trials'
+    assert np.all(raster_lines[0].get_ydata() == 0.0), \
+            'visualizations.raster_and_psth did not correctly label rasters'
+    assert raster_lines[1].get_xdata().size == 4, \
+            'visualizations.raster_and_psth did not correctly split rasters into trials'
+    assert raster_lines[1].get_ydata().size == 4, \
+            'visualizations.raster_and_psth did not correctly split rasters into trials'
+    assert np.all(raster_lines[1].get_ydata() == 1.0), \
+            'visualizations.raster_and_psth did not correctly label rasters'
 
 def test_play_sta():
-    """Test playing an STA as a movie.
-    
-    Matplotlib doesn't yet have a way to compare movies, and the formats
-    and precise bytes written by different encoding libraries are too variable
-    to test reliably. Instead, we write a specific frame of the animation
-    to disk as an image, and compare it with a baseline.
-    """
-    _comparison_wrapper('sta-movie-frame.png', utils.sta_movie_frame_saver)
+    """Test playing an STA as a movie by comparing a known frame."""
+    sta = utils.create_default_fake_filter()[-1]
+    sta -= sta.mean()
+    frame = utils.get_default_movie_frame()
+    animation = visualizations.play_sta(sta)
+    animation._func(frame)
+    imgdata = plt.findobj(plt.gcf(), AxesImage)[0].get_array()
+    imgdata -= imgdata.mean()
+    data = sta[frame, ...]
+    data -= data.mean()
+    assert np.allclose(imgdata, data), \
+            'visualizations.play_sta did not animate the 3D sta correctly.'
 
 
 def test_ellipse():
     """Test plotting an ellipse fitted to an RF."""
-    _comparison_wrapper('ellipse.png', utils.ellipse_saver)
+    temporal_filter, spatial_filter, sta = utils.create_default_fake_filter()
+    fig, ax = visualizations.ellipse(sta)
+    el = plt.findobj(ax, Ellipse)[0]
+    assert np.allclose(el.center, np.array(spatial_filter.shape) / 2.0), \
+            'visualizations.ellipse did not compute correct ellipse center'
+    assert np.allclose((el.height, el.width), 2.827082246), \
+            'visualizations.ellipse computed incorrect width and/or height'
     
 
 def test_plot_cells():
     """Test plotting ellipses for multiple cells on the same axes."""
-    _comparison_wrapper('plotcells.png', utils.plot_cells_saver)
+    ncells = 2
+    stas = [utils.create_default_fake_filter()[-1] for 
+            _ in range(ncells)]
+    np.random.seed(0)
+    visualizations.plot_cells(stas)
+
+    ellipses = plt.findobj(plt.gca(), Ellipse)
+    for el in ellipses:
+        assert np.allclose(el.center, utils.get_default_filter_size()[0] / 2.), \
+                'visualizations.plot_cells did not compute correct ellipse center'
+        assert np.allclose((el.height, el.width), 2.827082246), \
+                'visualizations.plot_cells computed incorrect width and/or height'
 
 
 def test_play_rates():
     """Test playing firing rates for cells as a movie."""
-    _comparison_wrapper('rates-movie-frame.png', utils.play_rates_saver)
+    sta = utils.create_default_fake_filter()[-1]
+    rates = utils.create_default_fake_rates()
+    fig, axes = visualizations.ellipse(sta)
+    patch = plt.findobj(axes, Ellipse)[0]
+    animation = visualizations.play_rates(rates, patch)
+
+    frame = utils.get_default_movie_frame()
+    animation._func(frame)
+    cmap = plt.cm.gray(np.arange(255))
+    desired_color = cmap[int(rates[frame] / rates.max())]
+    assert np.all(patch.get_facecolor()[:3] == desired_color[:3]), \
+            'visualizations.play_rates did not set patch color correctly'
 
 
 def test_anim_to_html():
@@ -117,18 +223,6 @@ def test_anim_to_html():
         pytest.skip('Cannot convert movie to HTML without IPython.')
 
     sta = utils.create_default_fake_filter()[-1]
-    html = viz.anim_to_html(viz.play_sta(sta))
+    html = visualizations.anim_to_html(visualizations.play_sta(sta))
     assert isinstance(html, HTML)
-
-
-def setup_module():
-    """Module-level setup, make the test images directory."""
-    if not os.path.exists(utils.get_test_image_dir()):
-        os.mkdir(utils.get_test_image_dir())
-
-
-def teardown_module():
-    """Module-level cleanup, remove the test images directory."""
-    if os.path.exists(utils.get_test_image_dir()):
-        os.rmdir(utils.get_test_image_dir())
 
--- tests/utils.py
+++ tests/utils.py
@@ -6,31 +6,9 @@ Some general utilities used in various testing routines.
 import os
 
 import numpy as np
-from matplotlib.patches import Ellipse
-import matplotlib.pyplot as plt
 
 from pyret.filtertools import _gaussian_function
 from pyret import spiketools
-from pyret import visualizations
-
-def get_image_filenames():
-    """Return the filenames of all images used in the visualizations tests."""
-    return (
-        )
-
-def get_image_dir():
-    """Return the directory containing all baseline and test images."""
-    return os.path.join(os.path.dirname(__file__), 'test-images')
-
-
-def get_baseline_image_dir():
-    """Return the directory containing all baseline images."""
-    return os.path.join(get_image_dir(), 'baseline')
-
-
-def get_test_image_dir():
-    """Return the directory containing all test images."""
-    return os.path.join(get_image_dir(), 'test')
 
 
 def get_default_filter_size():
@@ -64,126 +42,6 @@ def create_default_fake_rates():
     binned = spiketools.binspikes(spikes, time)
     rate = spiketools.estfr(binned, time)
     return rate
-
-
-def temporal_filter_saver(path):
-    """Save the output of pyret.visualizations.temporal to the given file."""
-    sta = create_default_fake_filter()[-1]
-    time = np.arange(sta.shape[0])
-    visualizations.temporal(time, sta)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def spatial_filter_saver(path):
-    """Save the output of pyret.visualizations.spatial to the given file."""
-    sta = create_default_fake_filter()[-1]
-    visualizations.spatial(sta)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def temporal_from_spatiotemporal_filter_saver(path):
-    """Save the output of pyret.visualizations.plot_sta when given only
-    a temporal filter to the given file.
-    """
-    temporal = create_default_fake_filter()[0]
-    time = np.arange(temporal.size)
-    visualizations.plot_sta(time, temporal)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def spatial_from_spatiotemporal_filter_saver(path):
-    """Save the output of pyret.visualizations.plot_sta when given only
-    a spatial filter to the given file.
-    """
-    temporal, spatial, _ = create_default_fake_filter()
-    time = np.arange(temporal.size)
-    visualizations.plot_sta(time, spatial)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def spatiotemporal_filter_saver(path):
-    """Save the output of pyret.visualizations.plot_sta when given a full
-    spatiotemporal filter to the given file.
-    """
-    temporal, _, sta = create_default_fake_filter()
-    time = np.arange(temporal.size)
-    visualizations.plot_sta(time, sta)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def raster_saver(path):
-    """Save the output of pyret.visualizations.raster to the given file."""
-    spikes, labels = create_default_fake_spikes()
-    visualizations.raster(spikes, labels)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def psth_saver(path):
-    """Save the output of pyret.visualizations.psth to the given file."""
-    spikes, _ = create_default_fake_spikes()
-    visualizations.psth(spikes, trial_length=5.0)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def raster_and_psth_saver(path):
-    """Save the output of pyret.visualizations.raster_and_psth to the given file."""
-    spikes, _ = create_default_fake_spikes()
-    visualizations.raster_and_psth(spikes, trial_length=5.0)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def sta_movie_frame_saver(path):
-    """Save the a single frame from pyret.visualizations.play_sta to the
-    given file.
-    """
-    sta = create_default_fake_filter()[-1]
-    frame = get_default_movie_frame()
-    animation = visualizations.play_sta(sta)
-    animation._func(frame)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def ellipse_saver(path):
-    """Save the output of pyret.visualizations.ellipse to the given file."""
-    sta = create_default_fake_filter()[-1]
-    visualizations.ellipse(sta)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def plot_cells_saver(path):
-    """Save the output of pyret.visualizations.plot_cells to the given file."""
-    ncells = 2
-    stas = [create_default_fake_filter()[-1] for _ in range(ncells)]
-    np.random.seed(0)
-    visualizations.plot_cells(stas)
-    plt.savefig(path)
-    plt.close(plt.gcf())
-
-
-def play_rates_saver(path):
-    """Save the a single frame from pyret.visualizations.play_rates to the
-    given file.
-    """
-    sta = create_default_fake_filter()[-1]
-    rates = create_default_fake_rates()
-
-    fig, axes = visualizations.ellipse(sta)
-    patch = plt.findobj(axes, Ellipse)[0]
-    animation = visualizations.play_rates(rates, patch)
-    frame = get_default_movie_frame()
-    animation._func(frame)
-    plt.savefig(path)
-    plt.close(fig)
 
 
 def create_temporal_filter(n, norm=True):
